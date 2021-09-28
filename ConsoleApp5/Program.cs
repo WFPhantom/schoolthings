ae915a1681b63c236173389c5fc22dedcb9f0fec
Veselin Plochev <vesko128@yahoo.com>
2021-09-28 18:52:05 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+string figure; double a; double b; double h; double area; double r; double p = 3.14;
+
+Console.WriteLine("figure: ");
+figure = Console.ReadLine();
+if (figure == "trapezoid")
+{
+    Console.Write("a = ");
+    a = double.Parse(Console.ReadLine());
+    Console.Write("b = ");
+    b = double.Parse(Console.ReadLine());
+    Console.Write("h = ");
+    h = double.Parse(Console.ReadLine());
+    area = ((a + b) / 2) * h;
+    Console.WriteLine(area);
+    Console.ReadLine();
+}
+else if (figure == "rectangle")
+{
+    Console.Write("a = ");
+    a = double.Parse(Console.ReadLine());
+    Console.Write("b = ");
+    b = double.Parse(Console.ReadLine());
+    area = a * b;
+    Console.WriteLine(area);
+    Console.ReadLine();
+}
+else if (figure == "circle")
+{
+    Console.Write("r = ");
+    r = double.Parse(Console.ReadLine());
+    double sqr = Math.Pow(r, 2);
+    area = sqr * p;
+    Console.WriteLine(area);
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Not supported figure");
+    Console.ReadLine();
+}
